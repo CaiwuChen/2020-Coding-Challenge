f734615888cfc6fdcd350f7d6a58d38a9c511dbf
unknown <cc4786@columbia.edu>
2024-04-01 14:52:50 -0400

    change on scoreboard.js and server.py

--- server.py
+++ server.py
@@ -50,7 +50,7 @@ def increase_score():
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
-
+    scoreboard.sort(key=lambda x: (-x["score"], x["name"]))
     return jsonify(scoreboard=scoreboard)
 
 
